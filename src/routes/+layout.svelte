dad9cdb849f71f8c8a79fa34f4da4006fe5e1879
Luke <lukewhrit@pm.me>
2024-08-15 23:28:02 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import '../app.scss';
+</script>
+
+<svelte:head>
+  <title>orca/pages</title>
+  <meta name="description" content="Stupid simple web hosting." />
+</svelte:head>
+
+<div>
+  <main>
+    <slot></slot>
+  </main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div id="center">
+  <h1>Stupid simple web hosting is coming soon.</h1>
+
+  <div id="links">
+    <a href="https://github.com/orca-group/" class="button" target="_blank">Github</a>
+    <a href="https://orca-group.github.io" class="button" target="_blank">Orca Studio</a>
+    <a href="/about" class="button" target="_blank">About</a>
+  </div>
+</div>
